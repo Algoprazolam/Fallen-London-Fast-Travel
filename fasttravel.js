0bdacf4f3fcaa6bcb570808b7ff505d695066594
Maya <verwerftmaya@gmail.com>
2023-09-21 18:44:08 +0200

    did some cleanup and made the code not overwrite the existing value of the travel button

--- fasttravel.js
+++ fasttravel.js
@@ -1,53 +1,17 @@
-console.log("hallo");
-
-
-function findroot() {
-  console.log("starting findroot function");
-  let getroot = document.getElementById("root");
-getroot.style.color = "red";
-console.log(getroot);
-console.log("findroot function ended");
-};
-
-
-console.log("calling the findroot function");
-findroot();
- 
-
-
-
-
-
+const travelButtonPara = document.createElement("p");
+  travelButtonPara.classList.add("travelButtonPara");
+  const travelButtonNode = document.createTextNode("Fast Travel Enabled");
+  travelButtonPara.appendChild(travelButtonNode);
 
 setTimeout(() => {
   console.log("calling changetext function")
   changeText()
-}, "1000");
+}, "2000");
 
-  
-  function changeText() {
-    console.log("starting changetext function");
-    const travelbutton = window.parent.document.querySelector(".travel-button--infobar");
-    console.log(travelbutton);
-    travelbutton.innerText = "Miauw";
-    console.log("changetext function ended ");
-  };
-  
-  
-
-//window.addEventListener('DOMContentLoaded', onLoad)
-//const onLoad = () => {}
-
-
-console.log("done");
-
-
-
-
-
-
-// changeText();
-
-// function changeText() {
-//   travelbutton.innerHTML = "found";
-// };
+function changeText() {
+  console.log("starting changetext function");
+  const travelButton = window.parent.document.querySelector(".travel-button--infobar");
+  console.log(travelButton);  
+  travelButton.appendChild(travelButtonPara);
+  console.log("changetext function ended ");
+};
