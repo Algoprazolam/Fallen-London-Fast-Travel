409038547c97c7c5c40e9d961b85d6101baac155
algoprazolam <algoprazolam@gmail.com>
2023-09-21 22:46:34 +0200

    travel button updates on url change

--- fasttravel.js
+++ fasttravel.js
@@ -2,12 +2,31 @@ let travelButtonPara = document.createElement("p");
 travelButtonPara.classList.add("travelButtonPara");
 let travelButtonNode = document.createTextNode("Fast Travel Enabled");
 travelButtonPara.appendChild(travelButtonNode);
+let previousUrl = "";
+const urlChangeConfig = {subtree: true, childList: true};
+
+const urlChangeChecker = new MutationObserver( function() {
+  console.log("mutenobserv does coco");
+  // or || (&& is and) smth else
+  if (window.location.href !== previousUrl) {
+    previousUrl = window.location.href;
+    changeText();
+  }
+});
 
 function changeText() {
-  const travelButton = window.parent.document.querySelector(".travel-button--infobar");
+  console.log("starting changetext function");
+  const travelButton = window.document.querySelector(".travel-button--infobar");
   travelButton.appendChild(travelButtonPara);
+  travelButton.disabled = false;
 };
 
-setTimeout(() => {
-  changeText()
-}, "2000");
+urlChangeChecker.observe(document, urlChangeConfig);
+
+//setinterval
+//setTimeout(() => {
+//  changeText()
+//}, "2000");
+
+
+
