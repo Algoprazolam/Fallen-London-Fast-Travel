6af66a2d297517fca4134df003de1fd377045057
Maya <verwerftmaya@gmail.com>
2023-09-21 19:02:13 +0200

    small cleanup

--- fasttravel.js
+++ fasttravel.js
@@ -1,17 +1,13 @@
-const travelButtonPara = document.createElement("p");
-  travelButtonPara.classList.add("travelButtonPara");
-  const travelButtonNode = document.createTextNode("Fast Travel Enabled");
-  travelButtonPara.appendChild(travelButtonNode);
-
-setTimeout(() => {
-  console.log("calling changetext function")
-  changeText()
-}, "2000");
+let travelButtonPara = document.createElement("p");
+travelButtonPara.classList.add("travelButtonPara");
+let travelButtonNode = document.createTextNode("Fast Travel Enabled");
+travelButtonPara.appendChild(travelButtonNode);
 
 function changeText() {
-  console.log("starting changetext function");
   const travelButton = window.parent.document.querySelector(".travel-button--infobar");
-  console.log(travelButton);  
   travelButton.appendChild(travelButtonPara);
-  console.log("changetext function ended ");
 };
+
+setTimeout(() => {
+  changeText()
+}, "2000");
