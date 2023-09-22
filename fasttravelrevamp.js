2adc3739ac7c41a346b86cd6094da7a900d087aa
algoprazolam <algoprazolam@gmail.com>
2023-09-22 11:50:11 +0200

    it now from anywhere goes to story and backs out of any possible RatherNot options

--- fasttravelrevamp.js
+++ fasttravelrevamp.js
@@ -1,5 +1,6 @@
 const checkerConfig = { subtree: true, childList: true };
 const storyPage = "https://www.fallenlondon.com/";
+let perhapsNotCheck = false;
 
 let travelButtonDisabledChecker = new MutationObserver(function () {
         let travelButtonGlobal = window.document.querySelector(".travel-button--infobar");
@@ -10,8 +11,16 @@ let travelButtonDisabledChecker = new MutationObserver(function () {
             travelButtonGlobal.addEventListener("click", function ()  {
                 if (window.location.href != storyPage) {
                     window.document.querySelector('[data-name="story"] a').click();
+                    perhapsNotCheck = true;
                 }
             })
+        }
+        if (perhapsNotCheck) {
+            let perhapsNotButton = window.document.querySelector(".buttons--storylet-exit-options button");
+            if (perhapsNotButton != null) {
+                perhapsNotButton.click();
+                perhapsNotCheck = false;
+            }
         }
 });     
         
