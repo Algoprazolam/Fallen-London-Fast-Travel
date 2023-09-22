ec4758d996f02c2c0d5c2faa70de552c66a634b8
algoprazolam <algoprazolam@gmail.com>
2023-09-22 11:37:45 +0200

    changed button from on url to page updates. doesn't require page refresh either. button redirects to story only for now

--- fasttravelrevamp.js
+++ fasttravelrevamp.js
@@ -1,22 +1,20 @@
 const checkerConfig = { subtree: true, childList: true };
 const storyPage = "https://www.fallenlondon.com/";
+let travelButtonGlobal = null;
 
 let travelButtonDisabledChecker = new MutationObserver(function () {
-    let travelButton = window.document.querySelector(".travel-button--infobar");
-    if (travelButton != null) {
-        travelButton.disabled = false;
-        travelButton.innerText = "Fast Travel";
-        travelButton.addEventListener("click", function ()  {
-            if (window.location.href != storyPage) {
-                window.document.querySelector('[data-name="story"] a').click();
-            }
-        })
-    }
-
-    // if (perhapsNotButton != null) {
-    //         perhapsNotButton.click();
-
-    // }
-});
-
+        travelButtonGlobal = window.document.querySelector(".travel-button--infobar");
+        if (travelButtonGlobal.disabled) {
+            console.log("Disabled")
+            travelButtonGlobal.disabled = false;
+            travelButtonGlobal.innerText = "Fast Travel";
+            travelButtonGlobal.addEventListener("click", function ()  {
+                if (window.location.href != storyPage) {
+                    window.document.querySelector('[data-name="story"] a').click();
+                }
+            })
+        }
+});     
+        
+    
 travelButtonDisabledChecker.observe(document, checkerConfig);
