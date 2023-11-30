a9649ce1ff26798cd3e7f7d8adfac0e898734c68
She <sheverwerft@gmail.com>
2023-11-30 14:42:22 +0100

    fixed the auto close map bug
    fixed the bug where if you back out of a storylet manually and then travel it would close the map one time

--- fasttravel.js
+++ fasttravel.js
@@ -4,12 +4,37 @@ let interval;
 let fastTravelling = false;
 const storyPage = "https://www.fallenlondon.com/";
 
+function startFastTravel() {
+    //when function is called set fastTravelling to true.
+    fastTravelling = true;
+    //if youre not on the story page it will go there by clicking the button on the page
+    //its a SPA so doing a normal redirect refreshes everything
+    if (window.location.href != storyPage) {
+        window.document.querySelector('[data-name="story"] a').click();
+    }
+};
+
+//clear the interval that runs the look for travel function
+function stopLookForTravel() {
+    clearInterval(interval);
+    interval = null;
+};
+// When the page changes
+const travelButtonDisabledChecker = new MutationObserver(function () {
+    // If not already looking for controls to modify, start lookin
+    if (interval == null) {
+        lookForTravel()
+    }
+});
+
+
 
 function lookForTravel() {
     // Check every few ms for...
     interval = setInterval(function () {
         // ... the travel button
         let travelButton = window.document.querySelector(".travel-button--infobar");
+        let perhapsNotButton = window.document.querySelector(".buttons--storylet-exit-options button");
         if (travelButton == null) {
             travelButton = window.document.querySelector(".travel-button--infobar");
         }
@@ -17,36 +42,34 @@ function lookForTravel() {
             // So it can be modified to say "fast travel" and a different event
             //stopLookForTravel()
             if (travelButton.disabled) {
-              travelButton.addEventListener("click", startFastTravel)
-
-            }
-
-
-            travelButton.disabled = false;
-            travelButton.innerText = "Fast Travel";
-                
+                travelButton.addEventListener("click", startFastTravel);
+                travelButton.disabled = false;
+                travelButton.innerText = "Fast Travel";
+            }          
         }
-        
-
-        
         // ... perhaps not button
-        let perhapsNotButton = window.document.querySelector(".buttons--storylet-exit-options button");
+        //if it is null loook for it
         if (perhapsNotButton == null) {
             perhapsNotButton = window.document.querySelector(".buttons--storylet-exit-options button");
         } else {
             // if there is a perhapsnotbutton
+            function removeListener() {
+                travelButton.removeEventListener("click", startFastTravel);   
+                travelButton.innerText="Travel";
+            };
+            perhapsNotButton.addEventListener("click", removeListener);
             // and you are fast travelling, click it
             if (fastTravelling) {
-                perhapsNotButton.click();
+                perhapsNotButton.click(); 
             }
         }
 
-        // ... if the url is storyPage AND you are fastTravelling AND there is no perhap sbutton
+        // ... if the url is storyPage AND you are fastTravelling AND there is no perhapsbutton
         if (window.location.href == storyPage && fastTravelling && perhapsNotButton == null) {
             // Click the travel button
-            travelButton.removeEventListener("click", startFastTravel )
             fastTravelling = false;
-            stopLookForTravel()
+            travelButton.removeEventListener("click", startFastTravel);           
+            stopLookForTravel();
             travelButton.click();
 
 
@@ -56,23 +79,6 @@ function lookForTravel() {
     }, 500);
 };
 
-function startFastTravel() {
-    fastTravelling = true;
-        if (window.location.href != storyPage) {
-            window.document.querySelector('[data-name="story"] a').click();
-        }
-};
 
-function stopLookForTravel() {
-    clearInterval(interval);
-    interval = null;
-}
-// When the page changes
-const travelButtonDisabledChecker = new MutationObserver(function () {
-    // If not already looking for controls to modify, start lookin
-    if (interval == null) {
-        lookForTravel()
-    }
-});
 
 travelButtonDisabledChecker.observe(document, checkerConfig);
